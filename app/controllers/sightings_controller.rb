5f89e56050c6de37872a5c00dbe8ace352293e84
Chris Kerbo <chris.kerbo@outlook.com>
2020-03-23 03:59:47 +0000

    Automatically backed up by Learn

--- app/controllers/sightings_controller.rb
+++ app/controllers/sightings_controller.rb
@@ -1,12 +1,12 @@
 class SightingsController < ApplicationController
 
   def index
-    @sightings = Sighting.all
+    sightings = Sighting.all
     render json: SightingSerializer.new(sightings)
   end
 
   def show
-    @sighting = Sighting.find(params[:id])
+    sighting = Sighting.find(params[:id])
     render json: SightingSerializer.new(sighting)
   end
 end
